196580a0440c0674b4ae2cfb9788bc8e5417d064
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-04-28 20:27:29 -0400

    Update addTwoNumbers.java

--- leetcode-solutions/addTwoNumbers.java
+++ leetcode-solutions/addTwoNumbers.java
@@ -1,15 +1,40 @@
 public class AddTwoNumbers {
 
-    // https://www.youtube.com/watch?v=LLPuC5kWD8U#action=share
+/*  
+    https://www.youtube.com/watch?v=LLPuC5kWD8U#action=share
 
-    //-------------------------------------- Time Complexity  O(m + n) -> m and n = number of nodes in l1 and l2 respectively. -------------------------------------------
+    Time Complexity:  O(m + n) 
+                      m and n = number of nodes in l1 and l2 respectively.    
+*/
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode head = result;
+        int carry = 0;
 
+        while(l1 != null || l2!= null || carry !=0){
+
+            int sum = ((l1 == null) ? 0 : l1.value ) + ((l2 == null)? 0 : l2.value) + carry; // If a node is null add 0 else node.value
+            result.next = new ListNode(sum % 10);
+            carry = sum /10;
+
+            if(l1 != null){
+                l1 = l1.next; // Advance l1
+            }
+
+            if(l2 !=null){
+                l2 = l2.next; // Advance l2
+            }
+
+            result = result.next; // Advance result
+        }
+        return head.next;
+    }
+    
     public static  class ListNode {
         public int value;
         public ListNode next;
-
-
+        
         public ListNode(int val) {
             this.value = val;
             this.next = null;
@@ -20,8 +45,8 @@ public class AddTwoNumbers {
             this.next = null;
         }
     }
-
-    public static void main(String args[]) {
+    
+     public static void main(String args[]) {
         ListNode list1 = new ListNode();
         // creating first list
         list1 = new ListNode(7);
@@ -44,51 +69,11 @@ public class AddTwoNumbers {
         print(rs);
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        ListNode head = result;
-        int carry = 0;
-
-        while(l1 != null || l2!= null || carry !=0){
-
-            int sum = ((l1==null) ? 0 : l1.value )+ ((l2==null)? 0 : l2.value) + carry;
-            result.next = new ListNode(sum%10);
-            carry = sum /10;
-
-            if(l1 != null){
-                l1 = l1.next;
-            }
-
-            if(l2 !=null){
-                l2 = l2.next;
-            }
-
-            result = result.next; // Advance result
-        }
-        return head.next;
-    }
-
-
-    /*ListNode head = new ListNode(0);
-        ListNode result = head;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry > 0) {
-            int resVal = (l1 != null? l1.val : 0) + (l2 != null? l2.val : 0) + carry;
-            result.next = new ListNode(resVal % 10);
-            carry = resVal / 10;
-            l1 = (l1 == null ? l1 : l1.next);
-            l2 = (l2 == null ? l2 : l2.next);
-            result = result.next;
-        }
-
-        return head.next;*/
-
     static void print(ListNode start) {
         ListNode ptr = start;
         while (ptr != null)
         {
-            System.out.println("Data = " + ptr.value);
+            System.out.println("Node = " + ptr.value);
             ptr = ptr.next;
         }
     }
