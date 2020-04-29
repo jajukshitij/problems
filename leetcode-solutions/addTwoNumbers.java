4a8120ecb248d692b891277215748fe58ab22b72
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-04-28 20:36:48 -0400

    formatting

--- leetcode-solutions/addTwoNumbers.java
+++ leetcode-solutions/addTwoNumbers.java
@@ -1,10 +1,22 @@
 public class AddTwoNumbers {
 
 /*  
+    Problem : Leetcode 2. Add Two Numbers
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example:
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807.
+    -----------------------------------------------------------------------------------------------------------
+    Reference:
     https://www.youtube.com/watch?v=LLPuC5kWD8U#action=share
 
-    Time Complexity:  O(m + n) 
-                      m and n = number of nodes in l1 and l2 respectively.    
+    ------------------------ Time Complexity: O(max(m,n)) ------------------------
+                                              m and n = number of nodes in l1 and l2, respectively.    
+    ------------------------ Space Complexity: O(max(m,n)) ------------------------
+                                               size of new result list.
 */
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -14,7 +26,8 @@ public class AddTwoNumbers {
 
         while(l1 != null || l2!= null || carry !=0){
 
-            int sum = ((l1 == null) ? 0 : l1.value ) + ((l2 == null)? 0 : l2.value) + carry; // If a node is null add 0 else node.value
+            int sum = ((l1 == null) ? 0 : l1.value ) + ((l2 == null)? 0 : l2.value) + carry; // If a node is null add 0 
+                                                                                             //else node.value
             result.next = new ListNode(sum % 10);
             carry = sum /10;
 
