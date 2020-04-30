efa05d1a47961b0a793a99aaef7dd551776680d9
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-04-29 23:07:47 -0400

    Update LongestPalindromicSubstring.java

--- leetcode-solutions/LongestPalindromicSubstring.java
+++ leetcode-solutions/LongestPalindromicSubstring.java
@@ -1,6 +1,5 @@
 /*
     Problem: Leetcode.5 Longest Palindromic Substring (Medium)
-    
     Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
     Example 1:
