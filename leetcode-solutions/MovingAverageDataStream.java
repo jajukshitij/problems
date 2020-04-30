c148073a58918af16d75df7adb0cb2553f3aecd7
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-04-30 02:20:04 -0400

    Create MovingAverageDataStream.java

---
+++ leetcode-solutions/MovingAverageDataStream.java
@@ -0,0 +1,49 @@
+/*
+    Problem: leetcode.346 Moving Average Data Stream (Easy)
+    Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+    
+    Example:
+    MovingAverage m = new MovingAverage(3);
+    m.next(1) = 1 // return 1.00000
+    m.next(10) = (1 + 10) / 2 // return 5.50000
+    m.next(3) = (1 + 10 + 3) / 3 // return 4.66667
+    m.next(5) = (10 + 3 + 5) / 3 // return 6.00000
+    ----------------------------------------------------------------------------------------------------------------------------------
+    
+    Reference:
+    https://www.youtube.com/watch?v=E-kjYOZEBxY
+    ----------------------------------------------------------------------------------------------------------------------------------
+    
+    ------------------------------------------------------- Time Complexity: O(1) ----------------------------------------------------
+    ------------------------------------------------------- Space Complexity: O(n) ---------------------------------------------------
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverageDataStream {
+    
+    int maxSize;
+    Queue<Integer> queue;
+    double sum;
+
+    public MovingAverageDataStream(int size){
+    
+        this.maxSize = size;
+        queue = new LinkedList<>();
+        sum = 0.0;
+    }
+
+    public double next(int val){
+    
+        queue.add(val);
+        sum += val;
+        
+        if(queue.size()>maxSize){
+            int remove =queue.poll();
+            sum -=remove;
+        }
+
+        return sum/queue.size();
+    }
+}
