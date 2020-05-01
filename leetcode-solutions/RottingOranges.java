dd10900896ad1f60d8f899fd515c24054d23d3da
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-04-30 23:22:01 -0400

    Create RottingOranges.java

---
+++ leetcode-solutions/RottingOranges.java
@@ -0,0 +1,102 @@
+/*
+    Problem: Leetcode.994 Rotting Oranges (Medium)
+    In a given grid, each cell can have one of three values:
+    the value 0 representing an empty cell;
+    the value 1 representing a fresh orange;
+    the value 2 representing a rotten orange.
+    Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+    Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1 instead.
+
+    Example 1:
+
+    Input: [[2,1,1],[1,1,0],[0,1,1]]
+    Output: 4
+    ---------------------------------------------------------------------------------------------------------------------------------
+    
+    Reference:
+    https://www.youtube.com/watch?v=TzoDDOj60zE
+    ---------------------------------------------------------------------------------------------------------------------------------
+    
+    --------------------------------------------------- Time Complexity: O(n) -------------------------------------------------------
+    -------------------------------------------------- Space Complexity: O(n) -------------------------------------------------------
+    Zombie Matrix
+    BFS
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RottingOranges {
+
+    public static int rottingOranges(int[][] grid){
+    
+        if(grid==null || grid.length==0){
+            return -1;
+        }
+        
+        int row= grid.length;
+        int col = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+            
+                if(grid[i][j]==2){
+                    queue.add(new int[]{i,j}); // adding Rotten orange cells to queue
+                    
+                }else if(grid[i][j]==1){
+                    count_fresh++; // Count of fresh oranges
+                }
+            }
+        }
+
+        if(count_fresh==0){ // No fresh oranges to rotten
+            return 0;
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}}; // 4 direction to traverse
+        int mins = 0;
+
+        while(!queue.isEmpty()){
+        
+            int size = queue.size();
+
+            for(int i=0; i<size; i++){
+            
+                int[] point = queue.poll(); // Picking rotten orange cell out of queue, to turn fresh oranges around it rotten
+
+                for(int[] dir : dirs){ // Traversing 4 direction around the rotten orange cell
+                
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+
+                    if(x<0 || y<0 || x>=row || y>=col || grid[x][y]!=1){ // If out of bound or not a fresh orange, continue
+                        continue;
+                    }
+                    
+                    grid[x][y] = 2; // Turn fresh ornage rotten
+                    queue.add(new int[]{x,y}); // add this rotten orange to queue to process later
+                    count_fresh--; // decrement count to keep track of fresh oranges left
+                }
+            }
+            mins++;
+        }
+
+        if(count_fresh==0){
+            return mins-1;
+        }else{ // All fresh oranges couldn't be reached
+            return -1;
+        }
+    }
+    
+    public static void main(String[] args){
+
+        int[][] grid ={ {2,1,1},
+                        {1,1,0},
+                        {0,1,1}};
+
+        System.out.println(rottingOranges(grid));
+    }
+}
