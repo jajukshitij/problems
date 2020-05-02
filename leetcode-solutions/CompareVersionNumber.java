1da6f7e311dd1230beb3bda271420a7af0cd4446
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-05-02 17:41:26 -0400

    Update CompareVersionNumber.java

--- leetcode-solutions/CompareVersionNumber.java
+++ leetcode-solutions/CompareVersionNumber.java
@@ -1,12 +1,14 @@
 /*
     Problem: Leetcode.165 Compare Version Number (Medium)
-    
     Compare two version numbers version1 and version2.
     If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
     You may assume that the version strings are non-empty and contain only digits and the . character.
     The . character does not represent a decimal point and is used to separate number sequences.
-    For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
-    You may assume the default revision number for each level of a version number to be 0. For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number. Its third and fourth level revision number are both 0.
+    For instance, 2.5 is not "two and a half" or "half way to version three",
+    it is the fifth second-level revision of the second first-level revision.
+    You may assume the default revision number for each level of a version number to be 0.
+    For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number.
+    Its third and fourth level revision number are both 0.
 
     Example 1:
     Input: version1 = "0.1", version2 = "1.1"
@@ -20,6 +22,7 @@
 public class CompareVersionNumber {
 
     public int compareVersionNumber(String version1, String version2){
+        
         String[] ver1 = version1.split("\\.");
         String[] ver2 = version2.split("\\.");
 
@@ -28,21 +31,22 @@ public class CompareVersionNumber {
         int v2 =0;
 
         for(int i=0; i<len; i++){
-            if(i<ver1.length){
+            
+            if(i < ver1.length){
                 v1= Integer.parseInt(ver1[i]);
             }else{
                 v1=0;
             }
 
-            if(i<ver2.length){
+            if(i < ver2.length){
                 v2= Integer.parseInt(ver2[i]);
             }else{
                 v2=0;
             }
 
-            if(v1>v2){
+            if(v1 > v2){
                 return 1;
-            }else if(v2>v1){
+            }else if(v2 > v1){
                 return -1;
             }
         }
