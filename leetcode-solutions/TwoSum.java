85ce4d6f52d6e534946290384e17ef63541d3b02
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-05-01 22:31:37 -0400

    Update TwoSum.java

--- leetcode-solutions/TwoSum.java
+++ leetcode-solutions/TwoSum.java
@@ -8,6 +8,21 @@
     Because nums[0] + nums[1] = 2 + 7 = 9,
     return [0, 1].
     --------------------------------------------------------------------------------------------------------------------------------
+    Problem: Leetcode. Two Sum II - Input Array is Sorted (Easy)
+    Given an array of integers that is already sorted in ascending order,
+    find two numbers such that they add up to a specific target number.
+    The function twoSum should return indices of the two numbers such that they add up to the target,
+    where index1 must be less than index2.
+
+    Note:
+    Your returned answers (both index1 and index2) are not zero-based.
+    You may assume that each input would have exactly one solution and you may not use the same element twice.
+    
+    Example:
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+    --------------------------------------------------------------------------------------------------------------------------------
 */
 
 import java.util.*;
@@ -15,7 +30,8 @@ import java.util.*;
 public class TwoSum {
 
     //--------------------------------------------- Time Complexity: O(n log (n)) -------------------------------------------------
-    //------------------------------------------------- Space Complexity: O(1) -----------------------------------------------------
+    //------------------------------------------------- Space Complexity: O(1) ----------------------------------------------------
+    //-------------------------------- Two Sum II - Input Array is Sorted (Time Complexity: O(n)) ---------------------------------
 
     public static int[] twoSumIndex(int[] nums, int target){
     
@@ -64,6 +80,42 @@ public class TwoSum {
         }
         
         return result;
+    }
+    
+    //--------------------------------------------- Time Complexity: O(n log (n)) -------------------------------------------------
+    //------------------------------------------------- Space Complexity: O(1) -----------------------------------------------------
+    
+    public static int twoSumCount (int[] nums, int target){
+        
+        Arrays.sort(nums);
+        
+        int i =0;
+        int j= nums.length-1;
+        int count =0;
+
+        while(i<j){
+            
+            if(nums[i]+nums[j] < target){
+                i++;
+            }else if (nums[i]+nums[j] > target){
+                j--;
+            }else{               
+                count++;
+                i++;
+                j--;
+                
+                while(i<j && nums[i]==nums[i-1]){
+                    i++;
+                }
+                
+                while(i<j && nums[j]==nums[j+1] ){
+                    j--;
+                }
+                
+            }
+        }
+        
+        return count;
     }
 
 }
