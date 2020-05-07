5871692d1e873c81fd16c3573a312e096edf8150
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2020-05-07 15:06:14 -0400

    Update AlienDictionaryVerify.java

--- leetcode-solutions/AlienDictionaryVerify.java
+++ leetcode-solutions/AlienDictionaryVerify.java
@@ -1,20 +1,22 @@
 /*
     Problem: Leetcode 953. Verifying an Alien Dictionary (Easy)
-    In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
-    Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+    In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order.
+    The order of the alphabet is some permutation of lowercase letters.
+    Given a sequence of words written in the alien language, and the order of the alphabet,
+    return true if and only if the given words are sorted lexicographicaly in this alien language.
     
     Example 1:
 
     Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
     Output: true
     Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
-    -------------------------------------------------------------------------------------------------------------------
+    ------------------------------------------------------------------------------------------------------------------------------------
     Reference: https://www.youtube.com/watch?v=qSbJZWENtX4
     
-    --------------------------- Time Complexity: O(C) -------------------------------------
+    ------------------------------------------------- Time Complexity: O(C) ------------------------------------------------------------
                                                  C = number of characters in words array
     
-    --------------------------- Space Complexity: O(1) -------------------------------------                     
+    ------------------------------------------------ Space Complexity: O(1) ------------------------------------------------------------                     
 */
 
 public class AlienDictionaryVerify {
