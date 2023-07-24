02f00b3dc2ff122a84077aa8f76576d93051ae6d
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2023-07-23 21:27:10 -0700

    Update ValidAnagram.java

--- leetcode-solutions/ValidAnagram.java
+++ leetcode-solutions/ValidAnagram.java
@@ -28,6 +28,26 @@ public class ValidAnagram {
 
     public static boolean isAnagram(String s, String t){
 
+
+        /*if(s.length()!=t.length()){
+            return false;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        
+        //int i = 0;
+        
+       /* for(int k=0; k<s.length(); k++){
+            if(sArray[i]!=tArray[i]){
+                return false;
+            }
+            i++;
+        }*/
+
+        
         if(s.length()!= t.length()){
             return false;
         }
