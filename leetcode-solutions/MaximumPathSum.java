0b82934959e012d8a60b29f1c5ef0b5b0956a996
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2023-08-12 14:47:38 -0700

    Update MaximumPathSum.java

--- leetcode-solutions/MaximumPathSum.java
+++ leetcode-solutions/MaximumPathSum.java
@@ -1,5 +1,5 @@
 /*
-    Problem: Leetcode.543 Binary Tree Maximum Path Sum (Hard)
+    Problem: Leetcode.124 Binary Tree Maximum Path Sum (Hard)
     Given a non-empty binary tree, find the maximum path sum.
     For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
     The path must contain at least one node and does not need to go through the root.
