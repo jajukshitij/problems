407ef3765f65bd499f3d5283f50e358e36c7e824
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2023-08-14 20:05:52 -0700

    Update DesignTicTacToe.java

--- leetcode-solutions/DesignTicTacToe.java
+++ leetcode-solutions/DesignTicTacToe.java
@@ -1,5 +1,5 @@
 /*
-    Problem: Leetcode.384 Design TicTacToe
+    Problem: Leetcode.348 Design TicTacToe
     Design a Tic-tac-toe game that is played between two players on a NxN grid.
     A move is guaranteed to be valid and is placed on an empty block. Once a winning condition is reached, no more moves is allowed.
     A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
