6d975c74c08819282e5ff25a50a6bbed31004304
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2024-12-05 11:32:42 -0800

    Create EmployeeFreeTime.java

---
+++ leetcode-solutions/EmployeeFreeTime.java
@@ -0,0 +1,14 @@
+public class EmployeeFreeTime {
+  /*
+  Problem: Leetcode 759 Employee Free Time (Hard)
+  We are given a list of employees, where each employee has a list of non-overlapping intervals that represent their working hours.
+  The intervals for each employee are sorted. Return a list of finite intervals representing free time for all employees,
+  i.e., the time intervals where no one is working.
+  
+  Example:
+  Input: schedule = [[[1, 2], [5, 6]], [[1, 3]], [[4, 10]]]
+  Output: [[3, 4]]
+  Explanation: There are total 3 employees, and all common free time would be [-inf, 1], [3,4], [10, inf].
+  We discard intervals as they aren't finite.
+  */
+}
