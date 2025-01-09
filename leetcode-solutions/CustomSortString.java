6aa616bf48151ccdb33de50a1771a5641543a088
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-01-09 12:39:16 -0800

    Update CustomSortString.java

--- leetcode-solutions/CustomSortString.java
+++ leetcode-solutions/CustomSortString.java
@@ -1,5 +1,5 @@
 /*
-    Problem: Leetcode. 791 Custom Sort String
+    Problem: Leetcode. 791 Custom Sort String (Medium)
     You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
     Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order,
     then x should occur before y in the permuted string. Return any permutation of s that satisfies this property.
