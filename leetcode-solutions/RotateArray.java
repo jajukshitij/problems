0eb12b30aee7d0c121823de50233640c367f389e
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-01-14 20:54:52 -0800

    Update RotateArray.java

--- leetcode-solutions/RotateArray.java
+++ leetcode-solutions/RotateArray.java
@@ -1,5 +1,5 @@
 /*
-    Problem: Leetcode.183 Rotate Array (Easy)
+    Problem: Leetcode.189 Rotate Array (Easy)
     Given an array, rotate the array to the right by k steps, where k is non-negative.
     Follow up:
     Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
