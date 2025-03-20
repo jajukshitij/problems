37b34fa20ee2b532d3c1569e425a3a8a5c4deb4d
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-03-19 18:29:39 -0700

    Update ValidPalindrome.java

--- leetcode-solutions/ValidPalindrome.java
+++ leetcode-solutions/ValidPalindrome.java
@@ -90,4 +90,41 @@ public class PalindromeCheck {
 
         System.out.println(isPalindrome(str));
     }
+
+    // -------------------------------------------------------- 680. Valid Palindrome II----------------------------------------------------
+    // Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+    // ------------------------------------------------------ Time Complexity: O(n) -------------------------------------------------
+    // ----------------------------------------------------- n = length of string ---------------------------------------------------
+    
+    public boolean validPalindrome2(String s) {
+        int i =0;
+        int j = s.length()-1;
+
+        while(i<j)
+        {
+            if(s.charAt(i)!= s.charAt(j))
+            {
+                return isPalindrome(s,i, j-1) || isPalindrome(s,i+1,j);
+            }
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String s, int i, int j)
+    {
+        while(i <j)
+        {
+            if(s.charAt(i)!= s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
 }
