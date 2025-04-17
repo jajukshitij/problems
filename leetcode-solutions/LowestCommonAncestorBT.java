d79642d668673d8184e0c6a3faa5c388fde5b5d2
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-04-16 22:07:36 -0700

    Update LowestCommonAncestorBT.java

--- leetcode-solutions/LowestCommonAncestorBT.java
+++ leetcode-solutions/LowestCommonAncestorBT.java
@@ -64,6 +64,21 @@ public class LowestCommonAncestor {
         return q;
     }
 
+// ------------------------------------------------------- Method 2: Recursive ----------------------------------------------------------
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if( root == p || root == q || root == null)
+            return root;
+        TreeNode left = lowestCommonAncestor( root.left,  p,  q);
+        TreeNode right = lowestCommonAncestor( root.right,  p,  q);
+        if(left == null)
+            return right;
+        else if (right == null)
+            return left;
+        else
+            return root;        
+    }
+
     public static  class Node {
         int value;
         Node left, right;
