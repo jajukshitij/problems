a0eb0523ddfb672710d64b2b6b33046b9701133a
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-04-17 12:06:16 -0700

    Update ContainerMostWater.java

--- leetcode-solutions/ContainerMostWater.java
+++ leetcode-solutions/ContainerMostWater.java
@@ -1,10 +1,20 @@
 /*
-    Problem: Leetcode.11 Container With Most Water (Medium)
+    Problem: Leetcode.11 Container With Most Water (Medium)    
+    You are given an integer array height of length n. There are n vertical lines drawn such that 
+    the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    Find two lines that together with the x-axis form a container, such that the container contains the most water.    
+    Return the maximum amount of water a container can store.
+    Note: You may not slant the container and n is at least 2.
+
+    Example 1:
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+    In this case, the max area of water (blue section) the container can contain is 49.
     
-    Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
-    n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
-    Find two lines, which together with x-axis forms a container, such that the container contains the most water.
-    Note: You may not slant the container and n is at least 2.    
+    Example 2:    
+    Input: height = [1,1]
+    Output: 1
     ------------------------------------------------------------------------------------------------------------------------------------
     
     Reference:
