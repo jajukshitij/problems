1349228ffa9a7f34815e49591bd780cc0b2cecb9
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-04-17 12:40:50 -0700

    Update ReorderLogFile.java

--- leetcode-solutions/ReorderLogFile.java
+++ leetcode-solutions/ReorderLogFile.java
@@ -1,19 +1,24 @@
 /*
-    Problem: Leetcode.937 Reorder Log Files (Easy)
-    You have an array of logs.  Each log is a space delimited string of words.
-    For each log, the first word in each log is an alphanumeric identifier.  Then, either:
-    Each word after the identifier will consist only of lowercase letters, or;
-    Each word after the identifier will consist only of digits.
-    We will call these two varieties of logs letter-logs and digit-logs.
-    It is guaranteed that each log has at least one word after its identifier.
-    Reorder the logs so that all of the letter-logs come before any digit-log.
-    The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.
-    The digit-logs should be put in their original order.
+    Problem: Leetcode.937 Reorder Data in Log Files (Medium)
+    You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+    
+    There are two types of logs:    
+    Letter-logs: All words (except the identifier) consist of lowercase English letters.
+    Digit-logs: All words (except the identifier) consist of digits.
+    
+    Reorder these logs so that:    
+    The letter-logs come before all digit-logs.
+    The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+    The digit-logs maintain their relative ordering.
+    
     Return the final order of the logs.
 
     Example 1:
     Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
     Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+    Explanation:
+    The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
+    The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
     -----------------------------------------------------------------------------------------------------------------------------------
     
     Reference:
