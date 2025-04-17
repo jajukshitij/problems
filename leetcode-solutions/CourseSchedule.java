ad5d6a07980c176e6fe8aa8102e2a0a2d262e183
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-04-17 10:29:34 -0700

    Update CourseSchedule.java

--- leetcode-solutions/CourseSchedule.java
+++ leetcode-solutions/CourseSchedule.java
@@ -91,4 +91,88 @@ public class CourseSchedule {
 
         return (count==numCourses);
     }
+
+//  --------------------------------------------------------------  Course Schedule 2 ----------------------------------------------------------
+    /*
+    Problem: Leetcode. 210 Course Schedule 2 (Medium)
+    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+    You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. 
+    If it is impossible to finish all courses, return an empty array.  
+    
+    Example 1:    
+    Input: numCourses = 2, prerequisites = [[1,0]]
+    Output: [0,1]
+    Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+    
+    Example 2:    
+    Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+    Output: [0,2,1,3]
+    Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. 
+    Both courses 1 and 2 should be taken after you finished course 0.
+    So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+    */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int[] prereq: prerequisites)
+        {
+            int pre = prereq[1];
+            int dep = prereq[0];
+
+            indegree[dep]++;
+            
+            if(!map.containsKey(pre))
+            {
+                map.put(pre, new ArrayList<Integer>());
+            }
+
+            map.get(pre).add(dep);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i =0; i<numCourses; i++)
+        {
+            if(indegree[i]==0)
+            {
+                queue.offer(i);
+            }
+        }
+
+        int[] res = new int[numCourses];
+        int index = 0;
+
+        while(!queue.isEmpty())
+        {
+            int curr = queue.poll();
+            res[index++] = curr;
+
+            List<Integer> deps = map.get(curr);
+
+            if(deps != null)
+            {
+                for(int dep: deps)
+                {
+                    indegree[dep]--;
+
+                    if(indegree[dep]==0)
+                    {
+                        queue.offer(dep);
+                    }
+                }
+            }
+        }
+
+        if(index == numCourses)
+        {
+            return res;
+        }
+        else
+        {
+            return new int[0];
+        }
+    }
 }
