06702936a82fc6144854516337fb5030940e1f8d
Kshitij Jaju <48064178+jajukshitij@users.noreply.github.com>
2025-05-15 21:54:07 -0700

    Update MinInRotatedSortedArray.java

--- leetcode-solutions/MinInRotatedSortedArray.java
+++ leetcode-solutions/MinInRotatedSortedArray.java
@@ -45,3 +45,34 @@ public class MinInRotatedSortedArray
         return nums[left];
     }
   }
+
+//------------------------------------------------------- Leetcode 154. Find Minimum in Rotated Sorted Array II (Hard)----------------------------------
+//---------------------------------------------------------------- Array contains duplicates -------------------------------------------------------------
+
+public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        if (nums[left] < nums[right])
+        {
+            return nums[left];
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        
+        return nums[right];
+    }
+
+
+
+
